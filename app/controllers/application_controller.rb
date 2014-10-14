f7ab498aad6773f95a91a04144627f49cc8ca1d3
Haukur Páll Hallvarðsson <hph@hph.is>
2014-10-14 20:05:31 +0000

    Redirect to previous page after log in; remove unecessary action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  after_action :store_location
 
-  def index
+  private
+
+  def store_location
+    ignored_pages = [new_user_session_path, new_user_registration_path]
+    unless ignored_pages.any? { |p| request.path.include?(p) } || request.xhr?
+      session[:previous_path] = request.fullpath if request.get?
+    end
+  end
+
+  def after_sign_in_path_for(resource)
+    session[:previous_path] || home_path
   end
 end
